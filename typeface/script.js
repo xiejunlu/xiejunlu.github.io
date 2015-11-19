33bf3530f02ac2028a3ddd83ab6b1b4af8995b7a
xiejunlu <xiejunluny@gmail.com>
2015-11-19 05:24:29 +0000

    msg ...

--- typeface/script.js
+++ typeface/script.js
@@ -9,18 +9,21 @@ $(function() {
     $(".draggable").draggable();
     $(".box").mousedown(function(e) {
       console.log(e.toElement);
+        $(".audio1").trigger('play');
         $('.box').append("<div class=\"" + e.toElement.classList[0] + " draggable\"> </div>");
         $(e.toElement).addClass("dragged");
         $(".draggable").draggable();
     });
     $(".clear").click(function() {
+        $(".audio3").trigger('play');
         $(".dragged").remove();
         $(".canvas").css("background-image", "url(canvasbg.jpg");
     });
 });
 
 $(".inspire").click(function() {
-  var randomNum = Math.floor(Math.random() * 4);
+  var randomNum = Math.floor(Math.random() * 8);
   console.log(randomNum);
+  $(".audio2").trigger('play');
   $(".canvas").css("background-image", "url(bg" + randomNum + ".png");
 });
